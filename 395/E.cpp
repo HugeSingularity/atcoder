71fee336cd76156c3441fb8b04e3d074a284149f
HugeSingularity <harry@alphashed.cn>
2025-03-07 20:15:04 +0800

    25/03/07

--- 395/E.cpp
+++ 395/E.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <algorithm>
 
 using namespace std;
 
 const long long inf = 9e18;
 vector <int> edge[2][200005];
 long long n, m, x, dp[200005][2];
+bool b[200005];
 
 void dfs(int u)
 {
-	if (dp[u][0] != inf) return;
+	if (dp[u][0] != inf || b[u]) return;
+	b[u] = 1;
 	for (auto& v : edge[0][u])
 	{
 		dfs(v);
@@ -20,14 +22,14 @@ void dfs(int u)
 		dfs(v);
 	}
 	for (auto& v : edge[0][u])
-	{
-		dp[u][0] = dp[v][0] + 1;
-		dp[u][1] = dp[v][0] + 1 + x;
-	}
-	for (auto& v : edge[1][u])
 	{
 		dp[u][0] = min(dp[u][0], dp[v][1] + 1 + x);
 		dp[u][1] = min(dp[u][1], dp[v][1] + 1);
+	}
+	for (auto& v : edge[1][u])
+	{
+		dp[u][0] = min(dp[u][0], dp[v][0] + 1);
+		dp[u][1] = min(dp[u][1], dp[v][0] + 1 + x);
 	}
 	return;
 }
@@ -42,7 +44,12 @@ int main()
 		edge[0][u].push_back(v);
 		edge[1][v].push_back(u);
 	}
-	memset(dp, inf, sizeof(dp));
+	for (int i = 1; i <= n; ++i)
+	{
+		dp[i][0] = inf;
+		dp[i][1] = inf;
+	}
+	dp[1][0] = 0, dp[1][1] = x;
 
 	dfs(n);
 	printf("%lld", min(dp[n][0], dp[n][1]));
